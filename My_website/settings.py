a1f7313e484504b28af122665e3e5e9d8a16651f
TKAMING <tobiaskaruth@gmail.com>
2022-11-25 17:05:26 +0100

    v.1.3

--- My_website/settings.py
+++ My_website/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = "django-insecure-2b0cqq%g4(!9is8((96n0m_%7lt!k47*cy&=0hirv4uritr&n%" #os.getenv("SECRET_KEY")#os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
